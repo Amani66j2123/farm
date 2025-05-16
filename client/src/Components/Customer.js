7bf63826590e54b7164d2dc184673532044592ba
Amani66j2123 <a7960317@gmail.com>
2025-05-17 00:25:52 +0400

    Farm app up

--- client/src/Components/Customer.js
+++ client/src/Components/Customer.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Axios from "axios";
+import * as ENV from "../config"; 
 
 const Customer = () => {
   const [fullName, setFullName] = useState("");
--- client/src/Components/UpdateCustomer.js
+++ client/src/Components/UpdateCustomer.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Axios from "axios";
 import { useParams } from "react-router-dom";
+import * as ENV from "../config"; 
 
 const UpdateCustomer = () => {
   const [fullName, setFullName] = useState("");
